6a978be739e8cfc35497342c784b390c41efc55c
tnbd <baodung2503@gmail.com>
2019-04-27 01:22:10 +0700

    update register form + validate

--- js/view.js
+++ js/view.js
@@ -1,43 +1,41 @@
 const view = {};
 
 view.setMessage = (elementId, message) => {
-    const element = document.getElementById(elementId);
-    if (element) {
-        element.innerText = message;
-    }
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.innerText = message;
+  }
 };
-view.setActiveScreen = () => {    
-    // const app = document.getElementById('app');
 
-    // if (app) {
-    //     app.innerHTML = components.index;
-    // }
+view.setActiveScreen = () => {
+  // const app = document.getElementById('app');
+  // if (app) {
+  //   app.innerHTML = components.index;
+  // }
 
-    //Listen submit event
-    const registerForm = document.getElementById('form-wrapper');
+  // Listen submit event
+  const registerForm = document.getElementById('form-wrapper');
+  if (registerForm) {
+    const handleSubmit = (event) => {
+      event.preventDefault();
 
-    if(registerForm) {
-        const handleSubmit = (event) => {
-            event.preventDefault(); //Cancel hanh dong mac dinh, khong tu refresh
-            
-            //get input values
-            const firstName = registerForm.firstName.value;
-            const lastName = registerForm.lastName.value;
-            const email = registerForm.email.value;
-            const password = registerForm.password.value;
-            const confirmPassword = registerForm.confirmpassword.value;
+      // get input values
+      const firstName = registerForm.firstName.value;
+      const lastName = registerForm.lastName.value;
+      const email = registerForm.email.value;
+      const password = registerForm.password.value;
+      const confirmPassword = registerForm.confirmPassword.value;
 
-            const registerInfo = {
-                firstName,
-                lastName,
-                email,
-                password,
-                confirmPassword,
-            };
+      const registerInfo = {
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmPassword,
+      };
 
-            controller.validateRegisterForm(registerInfo);
-
-        };
-        registerForm.addEventListener('submit', handleSubmit);
-    }
+      controller.validateRegisterForm(registerInfo);
+    };
+    registerForm.addEventListener('submit', handleSubmit);
+  }
 };
